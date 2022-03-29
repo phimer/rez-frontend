ac0f3e8a684ec971be0b3889f68d31bd8d32e441
Philip <philipmertz@live.de>
2022-03-29 13:07:44 +0200

    1.1.4 - fixed update and delete reipe bug

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -51,11 +51,11 @@ const Recipe = ({ recipe, onToggle, onDelete, onUpdate, errorMessage }) => {
                 {showFullRecipe && <Button className={'recipe-btn'} color={!showUpdateRecipe ? 'blue' : '#C67979'}
                     text={!showUpdateRecipe ? 'Update Recipe' : 'Close'} onClick={() => setShowUpdateRecipe(!showUpdateRecipe)}></Button>}
 
-                {showUpdateRecipe && <UpdateRecipe onUpdate={onUpdate} recipe={recipe} setShowUpdateRecipe={setShowUpdateRecipe} />}
+                {showUpdateRecipe && <UpdateRecipe onUpdate={onUpdate} recipe={fullRecipe} setShowUpdateRecipe={setShowUpdateRecipe} />}
 
                 {(confirmDeleteCheck) && <h4 className="recipe-error-message">{'Are you sure you want to delete this recipe?'}</h4>}
                 {(showFullRecipe && !showUpdateRecipe) && <Button className={'recipe-btn'} color={!confirmDeleteCheck ? 'red' : '#C67979'} text={!confirmDeleteCheck ? 'Delete Recipe' : 'No, do not delete'} onClick={() => setConfirmDeleteCheck(!confirmDeleteCheck)}></Button>}
-                {(showFullRecipe && !showUpdateRecipe && confirmDeleteCheck) && <Button className={'recipe-btn'} color={'red'} text={confirmDeleteCheck ? 'Yes, delete' : 'Delete Recipe'} onClick={() => { setConfirmDeleteCheck(false); onDelete(recipe) }}></Button>}
+                {(showFullRecipe && !showUpdateRecipe && confirmDeleteCheck) && <Button className={'recipe-btn'} color={'red'} text={confirmDeleteCheck ? 'Yes, delete' : 'Delete Recipe'} onClick={() => { setConfirmDeleteCheck(false); onDelete(fullRecipe) }}></Button>}
             </div>
         </div>
     );
