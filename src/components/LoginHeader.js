ea70e8086c9f77d8abcf10b9997754139a9884b3
Philip <philipmertz@live.de>
2022-03-29 11:37:29 +0200

    reworked login header

--- src/components/LoginHeader.js
+++ src/components/LoginHeader.js
@@ -3,19 +3,15 @@ import Button from './Button'
 
 const LoginHeader = ({ onLoginClick, showLogin, userLoggedIn, onLogout, currentUser }) => {
     return (
-        <div id='login-header'>
+        <div id="login-header">
+            {!userLoggedIn && <div id='login-div'>
+                {<Button className='login-btn' color={showLogin ? '#C67979' : '#86C679'} text={showLogin ? 'Close' : 'Login'} onClick={onLoginClick} />}
+            </div>}
 
+            {userLoggedIn && <div id='logout-div'>
+                {<h4 id='user-greeting'>Hello, {currentUser}</h4>}
 
-            {
-                !userLoggedIn && (
-                    <Button className='login-btn' color={showLogin ? '#C67979' : '#86C679'} text={showLogin ? 'Close' : 'Login'} onClick={onLoginClick} />)
-            }
-            {userLoggedIn && <div id='user-greeting-div'>
-                {userLoggedIn && <h4 id='user-greeting'>Hello, {currentUser}</h4>}
-                {
-                    userLoggedIn && (
-                        <Button className='login-btn' color={'#C67979'} text={'Logout'} onClick={onLogout} />)
-                }
+                {<Button className='login-btn logout-btn' color={'#C67979'} text={'Logout'} onClick={onLogout} />}
             </div>}
         </div>
     )
