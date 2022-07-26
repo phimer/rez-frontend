29b91696cc8f38da5e1c21f12cdd9e88fca66a05
Philip <philipmertz@live.de>
2022-07-26 16:52:14 +0200

    search under construction and header color

--- src/components/SearchRecipe.js
+++ src/components/SearchRecipe.js
@@ -1,8 +1,15 @@
 import React from 'react'
+// import underConstructionImage from './under-construction.png'
 
 const SearchRecipe = (errorMessage) => {
     return (
-        <div className='recipe-error-message'>Coming Soon</div>
+
+        <div className='search-coming-soon'>
+            <h2>Under Construction</h2>
+            <br></br>
+            <img src="under-construction.png" alt="Under Construction Image"></img>
+            <h2>Search Function Coming Christmas 2021!</h2>
+        </div>
     )
 }
 
